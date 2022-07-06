2fe4aa799a8e3bc701ab28d8047630dc4d779180
taleldayekh <dev@talel.io>
2022-07-06 16:12:19 +0200

    API request

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
+  const [dummyData, setDummyData] = useState()
+
+  useEffect(() => {
+    axios.get('https://jsonplaceholder.typicode.com/albums').then(res => setDummyData(res.data))
+  });
+
+  return dummyData ? (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        {dummyData.map(data => <li>{data.title}</li>)}
+      </ul>
     </div>
-  );
+  ) : (
+    <div>
+      <p>Loading dummy data...</p>
+    </div>
+  )
 }
 
 export default App;
